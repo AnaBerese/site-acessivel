88caf0ed6b1aa3d7d52fd63b5a2f368b7db522dd
AnaBerese <145561347+AnaBerese@users.noreply.github.com>
2025-03-10 09:19:55 -0300

    botao diminuir fonte

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded',function(){
  const aumentafontebotao. =document.getelementid.("aumentar-fonte");
+ const diminuirfontebotao. =document.getelementid.("diminuir-fonte");
+
  let tamanhoatualfonte = 1;
  aumentafontebotao.addEventListener('click', function){
  tamanhoatualfonte +=0.1;
- document.body.style.fontesize = $ [{tamanhoatualfonte}]
+ document.body.style.fontesize = $ '[{tamanhoatualfonte}]rem';
 
  };
 
 }
 );
+
+ let tamanhoatualfonte = 1;
+ diminuirfontebotao.addEventListener('click', function){
+ tamanhoatualfonte -=0.1;
+ document.body.style.fontesize = $ '[{tamanhoatualfonte}]rem';
+
+ };
+
+
