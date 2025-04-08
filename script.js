515610222ffef2c49eb944f4024d10f59f68f52b
AnaBerese <145561347+AnaBerese@users.noreply.github.com>
2025-04-08 10:45:03 +0000

    arrumando codigo

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(){
+
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-const alternaContraste = document.getElementById('alterna-contraste');
+    botaoDeAcessibilidade.addEventListener('click',function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    })
+
+    const alternaContraste = document.getElementById('alterna-contraste');
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
@@ -16,6 +25,5 @@ const alternaContraste = document.getElementById('alterna-contraste');
 
     alternaContraste.addEventListener('click', funcion(){
         document.body.classList.toggle('alto-contraste')
-
     })
 });
